9ee686b63ced93dd9b7f134b5b97636af312e2e7
Vladimir Klepov <vklepov@ozon.ru>
2023-10-08 12:39:19 +0300

    nicer upload button

--- src/app/Dashboard.tsx
+++ src/app/Dashboard.tsx
@@ -1,28 +1,36 @@
 import { Show, createResource, createSignal } from 'solid-js';
 import { parseCsv } from '../data/data';
+import { FiUpload } from 'solid-icons/fi';
+import './Dashboard.css';
+
+type WelcomeProps = { onUpload: (f: File) => void; loading: boolean };
+function Welcome(props: WelcomeProps) {
+  return (
+    <section class="grid-hero container grid-lg text-center">
+      <label class="btn btn-primary btn-lg" classList={{ loading: props.loading }}>
+        Upload CSV <FiUpload />
+        <input type="file" onInput={e => props.onUpload(e.currentTarget.files[0])} />
+      </label>
+    </section>
+  )
+}
 
 export function Dashboard() {
   const [file, setFile] = createSignal<File>(null);
   const [data] = createResource(file, file => file ? parseCsv(file) : null);
 
   return (
-    <>
-      {!file() && 
-        <input type="file" onInput={e => setFile(e.currentTarget.files[0])} />}
-      {file() && (
-        <Show when={data()} fallback={<div class="loading loading-lg" />}>
-        <table class="table">
-          <thead>
-            {data().cols.map(col => <th>{col.name}</th>)}
-          </thead>
-          <tbody>
-            {data().data.map(row => (
-              <tr>{row.map(val => <td>{val}</td>)}</tr>
-            ))}
-          </tbody>
-        </table>
-      </Show>
-      )}
-    </>
+    <Show when={data()} fallback={<Welcome onUpload={setFile} loading={!!file()} />}>
+      <table class="table">
+        <thead>
+          {data().cols.map(col => <th>{col.name}</th>)}
+        </thead>
+        <tbody>
+          {data().data.map(row => (
+            <tr>{row.map(val => <td>{val}</td>)}</tr>
+          ))}
+        </tbody>
+      </table>
+    </Show>
   );
 }
