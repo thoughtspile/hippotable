d90e230c12fb62478f6b486758cf607be1925d57
Vladimir Klepov <vklepov@ozon.ru>
2023-10-08 18:30:16 +0300

    use arquero

--- src/app/Dashboard.tsx
+++ src/app/Dashboard.tsx
@@ -2,6 +2,7 @@ import { Show, createResource, createSignal } from 'solid-js';
 import { parseCsv } from '../data/data';
 import { FiUpload } from 'solid-icons/fi';
 import './Dashboard.css';
+import type ColumnTable from 'arquero/dist/types/table/column-table';
 
 type WelcomeProps = { onUpload: (f: File) => void; loading: boolean };
 function Welcome(props: WelcomeProps) {
@@ -15,22 +16,30 @@ function Welcome(props: WelcomeProps) {
   )
 }
 
+function Table({ table }: { table: ColumnTable }) {
+  const cols = table.columnNames();
+  const indices = [...table.indices()];
+  return (
+    <table class="table">
+      <thead>
+        {cols.map(col => <th>{col}</th>)}
+      </thead>
+      <tbody>
+        {indices.map(i => (
+          <tr>{cols.map(col => <td>{table.get(col, i)}</td>)}</tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
 export function Dashboard() {
   const [file, setFile] = createSignal<File>(null);
-  const [data] = createResource(file, file => file ? parseCsv(file) : null);
+  const [table] = createResource(file, file => file ? parseCsv(file) : null);
 
   return (
-    <Show when={data()} fallback={<Welcome onUpload={setFile} loading={!!file()} />}>
-      <table class="table">
-        <thead>
-          {data().cols.map(col => <th>{col.name}</th>)}
-        </thead>
-        <tbody>
-          {data().data.map(row => (
-            <tr>{row.map(val => <td>{val}</td>)}</tr>
-          ))}
-        </tbody>
-      </table>
+    <Show when={table()} fallback={<Welcome onUpload={setFile} loading={!!file()} />}>
+      <Table table={table()} />
     </Show>
   );
 }
--- src/data/data.ts
+++ src/data/data.ts
@@ -1,24 +1,3 @@
-import Papa from 'papaparse';
+import { loadCSV } from 'arquero';
 
-interface DataTable {
-  length: number;
-  cols: {
-    name: string;
-  }[]
-  data: unknown[][];
-}
-
-export async function parseCsv(csv: File): Promise<DataTable> {
-  const res = await new Promise<Papa.ParseResult<unknown>>((ok) => {
-    Papa.parse(csv, { 
-      header: false,
-      complete: res => ok(res)
-    });
-  });
-  const [names, ...table] = res.data as [string[], ...unknown[][]];
-  return {
-    length: table.length,
-    cols: names.map(name => ({ name })),
-    data: table,
-  };
-}
+export const parseCsv = (csv: File) => loadCSV(URL.createObjectURL(csv), {});
