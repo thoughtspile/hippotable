4c7f0f585516f62b26224bae3ce34911ffc95893
Vladimir Klepov <vklepov@ozon.ru>
2023-11-19 13:15:57 +0300

    cleanup table

--- src/app/Table.tsx
+++ src/app/Table.tsx
@@ -5,36 +5,34 @@ import type ColumnTable from 'arquero/dist/types/table/column-table';
 import { FaSolidSort, FaSolidSortDown, FaSolidSortUp } from 'solid-icons/fa';
 import { createVirtualizer } from '@tanstack/solid-virtual';
 
-const Sort = (props: { dir: 'asc' | 'desc' | null }) => (
-  <>
-    {!props.dir && <FaSolidSort />}
-    {props.dir === 'desc' && <FaSolidSortUp />}
-    {props.dir === 'asc' && <FaSolidSortDown />}
-  </>
-);
+type Order = { col: string; dir: 'asc' | 'desc' };
 
 export function Table({ table }: { table: ColumnTable }) {
-  const [order, setOrder] = createSignal<{ col: string; dir: 'asc' | 'desc' }>({
-    col: null,
-    dir: 'asc',
-  });
-
-  const [colWidths, setColWidths] = createSignal(new Map<string, string>());
-  
-  const orderBy = (col: string) => {
+  const [order, setOrder] = createSignal<Order>({ col: null, dir: 'asc' });
+  function orderBy(col: string) {
     setOrder(o => ({ col, dir: col === o.col && o.dir === 'asc' ? 'desc' : 'asc' }));
-  };
-  
+  }
+
   const view = createMemo(() => {
     const { col, dir } = order();
     return col ? table.orderby(dir === 'desc' ? desc(col) : col) : table;
   });
 
-  const cols = table.columnNames();
+  return <TableView table={view()} orderBy={orderBy} order={order()} />;
+}
+
+type TableViewProps = {
+  table: ColumnTable;
+  order: Order;
+  orderBy: (c: string) => void;
+}
+function TableView(props: TableViewProps) {
+  const [colWidths, setColWidths] = createSignal(new Map<string, string>());
+  const cols = props.table.columnNames();
 
   let tableRef: HTMLTableElement;
   const virtualizer = createVirtualizer({
-    count: table.numRows() + 1,
+    count: props.table.numRows() + 1,
     getScrollElement: () => tableRef,
     estimateSize: () => 19,
     overscan: 5,
@@ -60,9 +58,9 @@ export function Table({ table }: { table: ColumnTable }) {
     <table ref={tableRef}>
       <thead>
         {cols.map(col => 
-          <th onClick={() => orderBy(col)} style={{ width: colWidths().get(col) }}>
+          <th onClick={() => props.orderBy(col)} style={{ width: colWidths().get(col) }}>
             {col}
-            <Sort dir={order().col === col ? order().dir : null} />
+            <Sort dir={props.order.col === col ? props.order.dir : null} />
           </th>
         )}
       </thead>
@@ -71,8 +69,7 @@ export function Table({ table }: { table: ColumnTable }) {
           <For each={cols}>{col => <td style={{ 'min-width': colWidths().get(col), "max-width": '1000px' }} />}</For>
         </tr>
         <For each={virtualizer.getVirtualItems().map((el) => el.index)}>{(index) => {
-          // subscribe to view updates (order / filter)
-          return <Row table={view} colWidths={colWidths()} cols={cols} index={index} />;
+          return <Row table={props.table} colWidths={colWidths()} cols={cols} index={index} />;
         }}</For>
         <tr style={{ height: `${remainingSize()}px` }}/>
       </tbody>
@@ -80,15 +77,30 @@ export function Table({ table }: { table: ColumnTable }) {
   )
 }
 
-type RowProps = { cols: string[], table: any, colWidths: any, index: number };
+type RowProps = { 
+  cols: string[];
+  table: ColumnTable;
+  colWidths: Map<string, string>;
+  index: number;
+};
 function Row(props: RowProps) {
   return (
     <tr>
       <For each={props.cols}>{col => {
         const style = { 'min-width': props.colWidths.get(col), "max-width": '1000px' };
-        return <td style={style}>{stringify(props.table().get(col, props.index))}</td>
+        return <td style={style}>{stringify(props.table.get(col, props.index))}</td>
       }}</For>
     </tr>
+  );
+}
+
+function Sort(props: { dir: 'asc' | 'desc' | null }) {
+  return (
+    <>
+      {!props.dir && <FaSolidSort />}
+      {props.dir === 'desc' && <FaSolidSortUp />}
+      {props.dir === 'asc' && <FaSolidSortDown />}
+    </>
   );
 }
 
